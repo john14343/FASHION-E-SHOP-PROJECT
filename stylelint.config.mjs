8bff3dabe01d2bb0586f8c982ab33f27726ff87d
john14343 <john129996@gmail.com>
2026-05-26 12:23:47 +0300

    Your message here

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+export default {
+  "extends": ["stylelint-config-standard"],
+  rules:{
+    "selector-class-pattern":null,
+    "keyframes-name-pattern":null,
+    "selector-id-pattern":null
+  }
+};
